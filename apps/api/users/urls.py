0303921acec99a78a78149e777fe44876ed6c9a7
Sahal-P <sahlcyprus@gmail.com>
2024-03-06 23:38:09 +0530

    feat: added sentance generating stream view and pathname counting celery task

--- apps/api/users/urls.py
+++ apps/api/users/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from users.views import UserProfileAPIView
+from users.views import UserProfileAPIView, StreamSentencesView
 
 urlpatterns = [
     path('', UserProfileAPIView.as_view(), name='user-profile-list-create'),
     path('<uuid:pk>/', UserProfileAPIView.as_view(), name='user-profile-detail'),
+    path('<uuid:pk>/', UserProfileAPIView.as_view(), name='user-profile-detail'),
+    path('stream', StreamSentencesView.as_view(), name='sse'),    
 ]
