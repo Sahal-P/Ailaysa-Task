d46bd15b5cf6ca7f6cecb8349f41465f90ca6683
Sahal-P <sahlcyprus@gmail.com>
2024-03-07 10:08:05 +0530

    updated production running scripts

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -7,5 +7,8 @@ echo "Current working directory: $current_dir"
 cd ./apps/api/core
 
 python manage.py runserver
+
+# gunicorn core.wsgi:application --bind 0.0.0.0:8000 --workers 3 --threads 3 --daemon
+# pkill gunicorn
 #  sudo service redis-server restart
 celery -A core worker -l info -P gevent
